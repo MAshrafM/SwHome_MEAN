590eb2ae595b3ce804696dbda7ff142b28ace711
MAshraf <ee.mashraf@gmail.com>
2018-11-05 16:18:27 +0200

    generate components

--- swhome-client/src/app/app.module.ts
+++ swhome-client/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms'
 
+// Services
+import { AuthRoutesService } from './services/auth-routes.service';
+
+// Components
 import { AppComponent } from './app.component';
+import { AddHomeComponent } from './add-home/add-home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddHomeComponent,
+    DashboardComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthRoutesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- swhome-client/src/app/services/auth-routes.service.ts
+++ swhome-client/src/app/services/auth-routes.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { enviroment } from '../../enviroments/enviroment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthRoutesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  login(){
+    return this.http.get(`${enviroment.BASE_URL}/api/login`);
+  }
 }
