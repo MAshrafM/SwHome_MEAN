a2d816bf6e07b488ec67791989596ed33ed9c54c
MAshraf <ee.mashraf@gmail.com>
2018-11-06 09:50:57 +0200

    add home client

--- swhome-client/src/app/add-home/add-home.component.ts
+++ swhome-client/src/app/add-home/add-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../environments/environment'
+
+const URL = `${environment.BASE_URL}/api/myhome`;
 
 @Component({
   selector: 'app-add-home',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-home.component.scss']
 })
 export class AddHomeComponent implements OnInit {
-
+  uploader: FileUploader = new FileUploader({
+    url: URL, itemAlias: 'file'
+  });
+  
+  address = {
+    street: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    country: ''
+  };
+  
+  newHome = {
+    home: '',
+    setting: '',
+    landscape: '',
+    bedrooms: '',
+    beds: '',
+    baths: '',
+    description: ''
+  };
+  
+  feedback: string;
+  
   constructor() { }
 
   ngOnInit() {
+    this.uploader.onSuccessItem = (item, res) => {
+      this.feedback = JSON.parse(res).message;
+    };
+    
+    this.uploader.onErrorItem = (item, res, status, headers) => {
+      this.feedback = JSON.parse(res).message;
+    };
+  }
+  
+  submit(){
+    this.uploader.onBuildItemForm = (item, form) => {
+      form.append('home', this.newHome.home);
+      form.append('setting', this.newHome.setting);
+      form.append('landscape', this.newHome.landscape);
+      form.append('bedrooms', this.newHome.bedrooms);
+      form.append('beds', this.newHome.beds);
+      form.append('baths', this.newHome.baths);
+      form.append('address', JSON.stringify(this.address));
+      form.append('description', this.newHome.description);
+    };
+    
+    this.uploader.uploadAll();
   }
 
 }
